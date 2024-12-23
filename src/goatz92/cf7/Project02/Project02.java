26038a5eaa5910778f907f4dff29f8910eba9d6b
goatz92 <goataragua@gmail.com>
2024-12-23 18:11:55 +0200

    rename project02

--- src/goatz92/cf7/Project02/Project02.java
+++
@@ -1,4 +0,0 @@
-package goatz92.cf7.Project02;
-
-public class Project02 {
-}
---
+++ src/goatz92/cf7/Project02/Project02Main.java
@@ -0,0 +1,9 @@
+package goatz92.cf7.Project02;
+
+public class Project02 {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
