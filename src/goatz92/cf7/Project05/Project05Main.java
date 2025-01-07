c9c2f86d9e5b6cabf1c86d97fa540de3d172646b
goatz92 <goataragua@gmail.com>
2025-01-07 18:03:25 +0200

    Started building Project05

--- src/goatz92/cf7/Project05/Project05Main.java
+++ src/goatz92/cf7/Project05/Project05Main.java
@@ -1,9 +1,55 @@
 package goatz92.cf7.Project05;
 
+import java.util.Scanner;
+
 public class Project05Main {
 
     public static void main(String[] args) {
 
+        boolean[][] seatMap = new boolean[30][12];
+        char row;
+        int columns;
+
+    }
+
+
+
+
+
+    public static void book(char row, int columns) {
+
+
+    }
+
+    public static void cancel(char row, int columns) {
 
     }
 }
+
+/*
+public static String[] columnInit (String[] column, char c) {
+    for (int i = 0; i < column.length; i++) {
+        if (c <= 'Z') {
+            column[i] = String.valueOf(c);
+        } else {
+            c = 'A';
+        }
+        if (i >= 26) {
+            column[i] = "" + c + c;
+        }
+        c++;
+    }
+    return column;
+}
+ */
+
+/*
+public static int[] rowInit (int[] row) {
+    int rowNum = 1;
+    for(int i = 0; i < row.length; i++){
+        row[i] = rowNum;
+        rowNum++;
+    }
+    return row;
+}
+ */
