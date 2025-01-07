89f4e350c052ff43cfd99522881c93b60ca04356
goatz92 <goataragua@gmail.com>
2025-01-07 20:21:53 +0200

    Added method for user input. Unfinished

--- src/goatz92/cf7/Project05/Project05Main.java
+++ src/goatz92/cf7/Project05/Project05Main.java
@@ -10,11 +10,32 @@ public class Project05Main {
         char row;
         int columns;
 
+
     }
 
 
 
 
+    public static char userChoiceRow (char row) throws Exception {
+
+        try(Scanner in = new Scanner(System.in)) {
+            row = in.next().charAt(0);
+            if (!Character.isLetter(row)) {
+                throw new Exception ("Character can't be a Symbol");
+            }
+            else if (Character.isWhitespace(row)) {
+                throw new Exception ("Character cant be Whitespace");
+            }
+            else if (Character.isDigit(row)){
+                throw new Exception ("Character can't be a digit")
+            }
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        while (row < 'A' )
+        return row;
+    }
 
     public static void book(char row, int columns) {
 
@@ -27,6 +48,7 @@ public class Project05Main {
 }
 
 /*
+
 public static String[] columnInit (String[] column, char c) {
     for (int i = 0; i < column.length; i++) {
         if (c <= 'Z') {
